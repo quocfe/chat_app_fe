2af5d7ebb847e2f9dc195b1d29c1abce7c02d0df
Quoc <phuquocfe@gmail.com>
2024-12-27 23:01:36 +0700

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
-import store, { persistor } from './redux/store.ts';
 import { SocketContextProvider } from './context/SocketContext.tsx';
-import 'react-toastify/dist/ReactToastify.css';
+import store, { persistor } from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<Provider store={store}>
