43905afe19bcf6aedffec57f943793e63bf6e82a
Quoc <phuquocfe@gmail.com>
2024-12-27 23:09:46 +0700

    fix cors

--- src/constant/ApiEndPoint.ts
+++ src/constant/ApiEndPoint.ts
@@ -1,9 +1,9 @@
 import { IFApiEndPoint } from '../models/ApiEndPoint';
 
 export const ApiEndPoint: IFApiEndPoint = {
-	auth: 'https://chat-app-be-xi.vercel.app/api//api/auth',
-	message: 'https://chat-app-be-xi.vercel.app/api//api/message',
-	replymessage: 'https://chat-app-be-xi.vercel.app/api//api/replymessage',
-	reactmessage: 'https://chat-app-be-xi.vercel.app/api//api/reactmessage',
-	users: 'https://chat-app-be-xi.vercel.app/api//api/users',
+	auth: 'https://chat-app-be-xi.vercel.app/api/api/auth',
+	message: 'https://chat-app-be-xi.vercel.app/api/api/message',
+	replymessage: 'https://chat-app-be-xi.vercel.app/api/api/replymessage',
+	reactmessage: 'https://chat-app-be-xi.vercel.app/api/api/reactmessage',
+	users: 'https://chat-app-be-xi.vercel.app/api/api/users',
 };
